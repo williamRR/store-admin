77b72cac03e5807fe045012298297c00403a5ad2
William Rehel <wrehel@falabella.cl>
2024-06-14 10:32:34 -0400

    chore: Update Vite config to deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/store-admin/',
+  base: 'https://williamRR.github.io/store-admin',
 });
